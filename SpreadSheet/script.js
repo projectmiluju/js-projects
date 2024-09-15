f2346362a71760058a0407449a11fdb94b6688ae
projectmiluju <dnjsdyd0712@naver.com>
2024-09-16 07:01:07 +0900

    js spreadsheet first model

---
+++ SpreadSheet/script.js
@@ -0,0 +1,116 @@
+const spreadSheetContainer = document.querySelector("#spreadsheet-container");
+
+const ROWS = 10;
+const COLS = 10;
+const spreadsheet = [];
+const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+class Cell {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    rowName,
+    columnName,
+    active = false
+  ) {
+    this.isHeader = isHeader;
+    this.disabled = disabled;
+    this.data = data;
+    this.row = row;
+    this.rowName = rowName;
+    this.column = column;
+    this.columnName = columnName;
+    this.active = active;
+  }
+}
+
+initSpreadsheet();
+
+function initSpreadsheet() {
+  for (let i = 0; i < ROWS; i++) {
+    let spreadsheetRow = [];
+    for (let j = 0; j < COLS; j++) {
+      let cellData = "";
+      let isHeader = false;
+      let disabled = false;
+
+      if (j === 0) {
+        cellData = i;
+        isHeader = true;
+        disabled = true;
+      }
+
+      if (i === 0) {
+        isHeader = true;
+        disabled = true;
+        cellData = alphabets[j - 1];
+      }
+
+      if (!cellData) {
+        cellData = "";
+      }
+
+      const rowName = i;
+      const columnName = alphabets[j - 1];
+
+      const cell = new Cell(isHeader, disabled, cellData, i, j, false);
+      spreadsheetRow.push(cell);
+    }
+    spreadsheet.push(spreadsheetRow);
+  }
+  drawSheet();
+  console.log(spreadsheet);
+}
+
+function createCellEl(cell) {
+  const cellEl = document.createElement("input");
+  cellEl.className = "cell";
+  cellEl.id = "cell_" + cell.row + cell.column;
+  cellEl.value = cell.data;
+  cellEl.disabled = cell.disabled;
+
+  if (cell.isHeader) {
+    cellEl.classList.add("header");
+  }
+  cellEl.onclick = () => handleCellClick(cell);
+
+  return cellEl;
+}
+
+function handleCellClick(cell) {
+  clearHeaderActiveStates();
+  const columnHeader = spreadsheet[0][cell.column];
+  const rowHeader = spreadsheet[cell.row][0];
+  const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+  const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+  columnHeaderEl.classList.add("active");
+  rowHeaderEl.classList.add("active");
+}
+
+function clearHeaderActiveStates() {
+  const headers = document.querySelectorAll(".header");
+
+  headers.forEach((header) => {
+    header.classList.remove("active");
+  });
+}
+
+function getElFromRowCol(row, col) {
+  return document.querySelector("#cell_" + row + col);
+}
+
+function drawSheet() {
+  for (let i = 0; i < spreadsheet.length; i++) {
+    const rowContainerEl = document.createElement("div");
+    rowContainerEl.className = "cell-row";
+
+    for (let j = 0; j < spreadsheet[i].length; j++) {
+      const cell = spreadsheet[i][j];
+      rowContainerEl.append(createCellEl(cell));
+    }
+    spreadSheetContainer.append(rowContainerEl);
+  }
+}
